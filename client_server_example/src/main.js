a910ace10c6e02850c70603da5d432c313927182
Aurelio Scarante <aurelioscarante25@gmail.com>
2022-05-28 13:20:47 -0300

    :rocket: setando portas via .env

---
+++ client_server_example/src/main.js
@@ -0,0 +1,23 @@
+import { createServer } from 'http';
+
+const server = createServer((req, res) => {
+  switch (req.url) {
+    case '/status': {
+      res.writeHead(200);
+      res.write('Hello World');
+      res.end();
+      break;
+    }
+    default: {
+      res.writeHead(404, 'Server not found');
+      res.end();
+    }
+  }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server is listening at http://${HOSTNAME}:${PORT}`)
+});
